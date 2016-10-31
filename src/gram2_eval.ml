95e6da3ea99a9cb4ab6b9019b453b7956959fb38
focs <yahyabindir@gmail.com>
2016-10-31 18:41:35 +0000

    Improved evaluations

--- src/gram2_eval.ml
+++ src/gram2_eval.ml
@@ -2,56 +2,50 @@ open Gram_defs
 
 let store = Hashtbl.create 100;;
 
-let rec eval_arithmetic = function
-	| Seq(e,f) -> let _ = eval_arithmetic e in let v = eval_arithmetic f in v
-	| Const(i) -> i
-	| Assign(Id (v),f) -> Hashtbl.replace store v (eval_arithmetic f);0
-	| Deref(Id (v)) -> let value = Hashtbl.find store v in (*print statements*) print_int(value); print_string("\n"); value
-	| Operator(op, e, f) -> match op with
-							|Plus -> eval_arithmetic e + eval_arithmetic f
-							|Minus -> eval_arithmetic e - eval_arithmetic f
-							|Times -> eval_arithmetic e * eval_arithmetic f
-							|Divide -> eval_arithmetic e / eval_arithmetic f;;
+let extract_int (Intres(i)) = i;;
+let extract_bool (Boolres(b)) = b;;
+let extract_string (Stringres(s)) = s;;
+let extract_unit (Unitres(u)) = u;;
 
-let rec eval_logic = function
-	| Not(e) -> let value = not (eval_logic e) in value
-	| Operator(op, e, f) -> match op with
-							|And -> eval_logic e && eval_logic f
-							|Or -> eval_logic e || eval_logic f;;
+let rec eval_exp = function
+	| Id(varname) -> Stringres(varname)
+	| Seq(exp1,exp2) -> let _ = eval_exp exp1 in
+				  let result = eval_exp exp2 in result
+	| Assign(var, exp) -> Unitres(Hashtbl.add store (extract_string(eval_exp var)) (eval_exp exp))
+	| Deref(var) -> Hashtbl.find store (extract_string(eval_exp var))
 
-let rec eval_compare = function
-	| Operator(op, e, f) -> match op with
-							|Leq -> eval_compare e <= eval_compare f
-							|Geq -> eval_compare e >= eval_compare f
-							|Equal -> eval_compare e == eval_compare f
-							|Noteq -> eval_compare e != eval_compare f;;
+	| True -> Boolres(true)
+	| False -> Boolres(false)
+	| Const(num) -> Intres(num)
 
-(*
-let rec eval_condition = function
-	| While(e,c) -> let boolean = eval_logic e in
-					let c' = eval_arithmetic c in
-					if boolean then eval_condition(While(e,c))
-								else ()
-	| If(e,c) -> let boolean = eval_logic e in
-				 let c' = eval_arithmetic c in
-				 if boolean then c' else ()
-	| Else(e,c,d) -> let boolean = eval_logic e in
-				   	 let c' = eval_arithmetic c in
-					 let d' = eval_arithmetic d in
-				   	 if boolean then c' else d';;
-*)
+	| Operator(Plus, exp1, exp2) -> Intres(extract_int(eval_exp exp1) + extract_int(eval_exp exp2))
+	| Operator(Minus, exp1, exp2) -> Intres(extract_int(eval_exp exp1) - extract_int(eval_exp exp2))
+	| Operator(Times, exp1, exp2) -> Intres(extract_int(eval_exp exp1) * extract_int(eval_exp exp2))
+	| Operator(Divide, exp1, exp2) -> Intres(extract_int(eval_exp exp1) / extract_int(eval_exp exp2))
+	| Not exp -> Boolres(not (extract_bool(eval_exp exp)))
+	| Operator(And, exp1, exp2) -> Boolres(extract_bool(eval_exp exp1) && extract_bool(eval_exp exp2))
+	| Operator(Or, exp1, exp2) -> Boolres(extract_bool(eval_exp exp1) || extract_bool(eval_exp exp2))
+	| Operator(Leq, exp1, exp2) -> Boolres(extract_int(eval_exp exp1) <= extract_int(eval_exp exp2))
+	| Operator(Geq, exp1, exp2) -> Boolres(extract_int(eval_exp exp1) >= extract_int(eval_exp exp2))
+	| Operator(Equal, exp1, exp2) -> Boolres(extract_int(eval_exp exp1) = extract_int(eval_exp exp2))
+	| Operator(Noteq, exp1, exp2) -> Boolres(extract_int(eval_exp exp1) != extract_int(eval_exp exp2))
+	| Printint(exp) -> Unitres(print_int(extract_int(eval_exp exp)); print_string("\n"))
+	
+	| Else(cond, exp1, exp2) -> if extract_bool(eval_exp cond) then eval_exp exp1 else eval_exp exp2
+	| If(cond, exp) -> if extract_bool(eval_exp cond) then eval_exp exp else Unitres(())
+	| While(cond, exp) -> Unitres(while extract_bool(eval_exp cond) do eval_exp exp done)
 
 let rec eval_other = function
 	| Let(v, e, c) -> failwith "Not implemented"
 	| New(v, e, c) -> failwith "Not implemented"
 	| Readint -> failwith "Not implemented"
-	| Printint(e) -> failwith "Not implemented"
 	| Apply(v, args) -> failwith "Not implemented";;
 
 let eval_func = function
-	| ("main", args, c) -> eval_arithmetic c
-	| (name, args, c) -> print_string("Other Function found \n");0;;
+	| ("main", args, exp) -> eval_exp exp
+	| (name, args, exp) -> Unitres(print_string("Other Function found \n"));;
 
 let rec eval_prog = function
-	| [] -> print_string("End Of File \n")
-	| (x::xs) -> eval_func x; eval_prog xs;;
+	| [] -> print_string("End Of File")
+	| (x::xs) -> let _ = eval_func x in 
+				 let program = eval_prog xs in program;;
