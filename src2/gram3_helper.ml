30e657c658b801663ab020598554cc1b7667ddc7
focs <yahyabindir@gmail.com>
2016-12-09 12:16:03 +0000

    Added 3 helper functions

--- src2/gram3_helper.ml
+++ src2/gram3_helper.ml
@@ -6,6 +6,7 @@ let getStr (Str(x)) = x;;
 let getUnit (Unit(x)) = x;;
 
 (* UGLY  but it works *)
+(* Using the comparison operators for both Bools and Ints*)
 let lessThan x y =
 	let (a,b) = (x,y) in
 	match (a,b) with
@@ -31,3 +32,43 @@ let notEqual x y =
 let rec lookfor env x = match env with
 	| [] -> Unit(())
 	| ((e,v)::xs) -> if x = e then v else lookfor xs x;;
+
+
+let string_of_operator op = match op with
+	| Plus -> "add "
+	| Minus -> "sub "
+	| Times -> "mul "
+	| Divide -> "div "
+
+let print_cmp addr1 addr2=
+	"ld r" ^ (string_of_int (getInt(addr1)))
+	^ "\nld r" ^ (string_of_int (getInt(addr2)))
+	^ "\ncmp r" ^ (string_of_int (getInt(addr1)))
+	^ " r" ^ (string_of_int (getInt(addr2)))
+	^ "\n";;
+
+(* Prints assembly for AND, only works for IF statements *)
+let print_and addr1 addr2=
+	"ld r" ^ (string_of_int (getInt(addr1)))
+	^ "\nld r" ^ (string_of_int (getInt(addr2)))
+	^ "\ncmp r" ^ (string_of_int (getInt(addr1)))
+	^ " 0 \njne NEXT \nj ELSE \nNEXT:" 
+	^ "\ncmp r" ^ (string_of_int (getInt(addr2)))
+	^ " 0 \nje " ^ "ELSE\n";;
+
+(* Prints assembly for OR, only works for IF statements *)
+let print_or addr1 addr2=
+	"ld r" ^ (string_of_int (getInt(addr1)))
+	^ "\nld r" ^ (string_of_int (getInt(addr2)))
+	^ "\ncmp r" ^ (string_of_int (getInt(addr1)))
+	^ " 0 \nje NEXT \nj IF \nNEXT:" 
+	^ "\ncmp r" ^ (string_of_int (getInt(addr2)))
+	^ " 0 \nje ELSE \nIF: \n";;
+
+
+let write_op n = match n with
+(*Leq*)		| 1 -> "jg " ^ "ELSE\n"
+(*Geq*)		| 2 -> "jl " ^ "ELSE\n"
+(*Equal*)	| 3 -> "jnz " ^ "ELSE\n"
+(*Noteq*)	| 4 -> "jz " ^ "ELSE\n"
+(*Do Nothing*)	| 5 -> ""
